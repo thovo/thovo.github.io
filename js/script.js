14d777be98021b19db120b794e89b98671280e69
thovo <votuongtho@gmail.com>
2015-09-21 16:23:17 +0200

    Update content and scroll to top button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function(){
+
+	// hide #back-top first
+	$("#back-to-top").hide();
+
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('#back-to-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+
+});
